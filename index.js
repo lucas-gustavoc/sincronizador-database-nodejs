8225bb92ea2bd60f741d993d3d860cb1ebd8106f
Lucas Gustavo <lucas.gustavoc@live.com>
2020-01-30 17:38:51 -0300

    Criação das primeiras rotas

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-const db = require('./db/mysql')
+// const db = require('./db/mysql')
+const express = require('express')
 
-console.log
+const app = express()
+const port = 3000
+
+app.use(express.json())
+
+app.get('/pedidos/:id', (req, res) => {
+    res.send('Aqui nós enviamos o pedido de id ' + req.params.id)
+})
+
+app.get('/pedidos/status/:status', (req, res) => {
+    res.send('Aqui nós enviamos todos os pedidos no status "' + req.params.status + '"')
+})
+
+app.patch('/pedidos/:id/status/:novostatus', (req, res) => {
+    res.send('Aqui nós vamos atualizar o status do pedido ' + req.params.id + ' para "' + req.params.novostatus + '"')
+})
+
+app.patch('/produtos/:sku/preco/:novopreco', (req, res) => {
+    res.send('Aqui nós vamos atualizar o valor do produto ' + req.params.sku + ' para ' + req.params.novopreco)
+})
+
+app.patch('/produtos/:sku/estoque/:novoestoque', (req, res) => {
+    res.send('Aqui nós vamos atualizar o estoque do produto ' + req.params.sku + ' para ' + req.params.novoestoque)
+})
+
+app.listen(port, () => {
+    console.log('Server listening on port ' + port)
+})
